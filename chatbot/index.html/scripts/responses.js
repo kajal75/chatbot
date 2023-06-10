039087d68928541c4403f2ff2d95c4b04daaf067
kajal75 <130081293+kajal75@users.noreply.github.com>
2023-06-10 13:29:00 +0530

    add with index.html file

---
+++ chatbot/index.html/scripts/responses.js
@@ -0,0 +1,25 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    } else if (input == "aur bata") {
+        return "Sab badhiyan";
+    } else if (input == "Tu bata") {
+        return "mera bhi";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+    
+}
